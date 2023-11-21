880c760426030d2de66cc936a6dcf7aae2b6a9e6
luliangliang <l1970374121@gmail.com>
2023-11-21 16:21:04 +0800

    增加二分查找

--- AlgorithmTest/Program.cs
+++ AlgorithmTest/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] aa = new int[] { 1,2,3,4,5,6,7,8};
+            int b=Search(aa, -1);
+            Console.WriteLine(b);
+        }
+        public static int Search(int[] ints, int Target)
+        {
+            
+            int left = 0;
+            int right=ints.Length-1;
+            int Middle;
+            //定搜索边界
+           while (left <= right) 
+            {
+                Middle =left+(right- left) / 2;
+                if (Target<ints[Middle]) 
+                {
+                    right = Middle-1;
+                }
+                if (Target> ints[Middle])
+                {
+                    left = Middle+ 1;
+                }
+                if (Target == ints[Middle])
+                {
+                    return Middle;
+                }
+            }
+            return -1;
         }
     }
+    
 }
