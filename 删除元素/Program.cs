4b102801ba8249987592faa4fd2d5823396c3a93
luliangliang <l1970374121@gmail.com>
2023-11-29 20:36:23 +0800

    添加链表/删除元素类问题/设计链表部分方法

---
+++ 删除元素/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace 删除元素
+{
+    #region 问题描述
+    /*题意：删除链表中等于给定值 val 的所有节点。
+      示例 1： 输入：head = [1,2,6,3,4,5,6], val = 6 输出：[1,2,3,4,5]
+      示例 2： 输入：head = [], val = 1 输出：[]
+      示例 3： 输入：head = [7,7,7,7], val = 7 输出：[]
+     */
+    #endregion
+    public class ListNode
+    {
+        public int Value;
+        public ListNode? Next;
+        public ListNode(int x,ListNode listNode)
+        {
+            Value = x;
+            Next = listNode;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solution(2);
+        }
+        public static ListNode Solution(ListNode head, int val)
+        {
+            //虚拟表头，占位
+            ListNode newHead = new ListNode(0, head);
+            ListNode? TempHead = head;
+            while (TempHead.Next!=null)
+            {
+                if (TempHead.Next.Value==val)
+                {
+                    TempHead.Next = TempHead.Next.Next;
+                }
+                else
+                {
+                    TempHead = TempHead.Next;
+                }
+            }
+            return newHead.Next;
+        }
+    }
+}
---
+++ 设计链表/Program.cs
@@ -0,0 +1,80 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace 设计链表
+{
+    internal class Program
+    {
+        #region 问题描述
+        /*在链表类中实现这些功能：
+          get(index)：获取链表中第 index 个节点的值。如果索引无效，则返回-1。
+          addAtHead(val)：在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+          addAtTail(val)：将值为 val 的节点追加到链表的最后一个元素。
+          addAtIndex(index,val)：在链表中的第 index 个节点之前添加值为 val  的节点。如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
+          deleteAtIndex(index)：如果索引 index 有效，则删除链表中的第 index 个节点。
+         */
+        #endregion
+       
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+    public class ListNode
+    {
+        public int Value;
+        public ListNode? Next;
+        public ListNode(int x)
+        {
+            Value = x;
+        }
+    }
+    public class MyLinkedList
+    {
+        /// <summary>
+        /// 虚拟表头
+        /// </summary>
+        private ListNode _virtualHead;
+        /// <summary>
+        /// 链表长度
+        /// </summary>
+        private int _count;
+       public  MyLinkedList() 
+        {
+            _virtualHead = new ListNode(0);
+            _count = 0;
+        }
+        /// <summary>
+        /// 获取链表中第 index 个节点的值。如果索引无效，则返回-1
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public int Get(int index)
+        {
+            ListNode tempListNode = _virtualHead;
+            if (index<0||index>_count-1)
+            {
+                return -1;
+            }
+            while (index>0) 
+            {
+                tempListNode=tempListNode.Next;
+                index--;
+            }
+            return tempListNode.Value;
+        }
+        /// <summary>
+        /// 将值为 val 的节点追加到链表的最后一个元素
+        /// </summary>
+        /// <param name="val"></param>
+        public void AddAtTail(int val)
+        {
+            ListNode tempListNode=_virtualHead;
+            while (tempListNode.Next!=null)
+            {
+                tempListNode= tempListNode.Next;
+            }
+            tempListNode.Next=new ListNode(val);
+            _count++;
+        }
+    }
+}
