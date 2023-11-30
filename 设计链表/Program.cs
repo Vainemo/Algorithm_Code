2ae0471361815d7703f7b15f518e343407565a0f
luliangliang <l1970374121@gmail.com>
2023-11-30 19:34:13 +0800

    补充设计链表的方法

--- 设计链表/Program.cs
+++ 设计链表/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Net.Mime.MediaTypeNames;
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace 设计链表
 {
@@ -60,7 +61,7 @@ namespace 设计链表
                 tempListNode=tempListNode.Next;
                 index--;
             }
-            return tempListNode.Value;
+            return tempListNode.Next.Value;
         }
         /// <summary>
         /// 将值为 val 的节点追加到链表的最后一个元素
@@ -75,6 +76,76 @@ namespace 设计链表
             }
             tempListNode.Next=new ListNode(val);
             _count++;
+        }
+        /// <summary>
+        /// 在链表的第一个元素之前添加一个值为 val 的节点。插入后，新节点将成为链表的第一个节点。
+        /// </summary>
+        /// <param name="val"></param>
+        public void AddAtHead(int val)
+        {
+            ListNode tempListNode = _virtualHead.Next;
+            if (tempListNode==null)
+            {
+                AddAtTail(val);
+            }
+            else
+            {
+                _virtualHead.Next = new ListNode(val);
+                _virtualHead.Next.Next= tempListNode;
+                _count++;
+            }
+        }
+        /// <summary>
+        /// 在链表中的第 index 个节点之前添加值为 val  的节点。如果 index 等于链表的长度，则该节点将附加到链表的末尾。如果 index 大于链表长度，则不会插入节点。如果index小于0，则在头部插入节点。
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="val"></param>
+        public void AddAtIndex(int index,int val)
+        {
+            if (index<0)
+            {
+                AddAtHead(val);
+            }
+           else  if (index>_count)
+            {
+                return;
+            }
+            else if (index==_count)
+            {
+                AddAtTail(val);
+            }
+            else
+            {
+                ListNode tempListNode = _virtualHead;
+                while (index>1)
+                {
+                    tempListNode= _virtualHead.Next;
+                    index--;
+                }
+                ListNode NextNode = _virtualHead.Next;
+                _virtualHead.Next= new ListNode(val);
+                _virtualHead.Next = NextNode;
+
+            }
+        }
+        /// <summary>
+        /// 如果索引 index 有效，则删除链表中的第 index 个节点。
+        /// </summary>
+        /// <param name="index"></param>
+        public void DeleteAtIndex(int index)
+        {
+            if (index<0||index>_count-1)
+            {
+                return;
+            }
+            ListNode tempListNode = _virtualHead;
+            while (index > 1)
+            {
+                tempListNode = _virtualHead.Next;
+                index--;
+            }
+            tempListNode.Next = tempListNode.Next.Next;
+            _count--;
         }
     }
 }
