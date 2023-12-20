6452c05940894882250a1a6585773e83bd3f59e7
luliangliang <l1970374121@gmail.com>
2023-12-20 19:22:08 +0800

    环形链表/数组作为哈希表寻找字母异位词

---
+++ 有效的字母异位词/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace 有效的字母异位词
+{
+    internal class Program
+    {
+        #region 问题描述
+        /*给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+         * 示例 1: 输入: s = "anagram", t = "nagaram" 输出: true
+         * 示例 2: 输入: s = "rat", t = "car" 输出: false
+         * 说明: 你可以假设字符串只包含小写字母。
+         */
+        #endregion
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static bool Solution(string s,string t)
+        {
+            if (s.Length!=t.Length) 
+            {
+                return false;
+            }
+            //记录各个字母出现的次数，在t中出现++，在i中出现--，最后==0说明完全抵消
+
+            int[] record= new int[26];
+            for (int i=0; i<s.Length;i++)
+            {
+                //26个字母减去第一个‘a’,数值必定落在26以内
+                record[t[i] -'a']++;
+                record[s[i] -'a']--;
+            }
+            for (int i = 0; i < record.Length; i++)
+            {
+                if (record[i]!=0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- 环形链表/Program.cs
+++ 环形链表/Program.cs
@@ -12,10 +12,42 @@
         {
            
         }
-        static int Solution(ListNode head)
+        static int? Solution(ListNode head)
         {
-            int pos = -1;
-            return pos;
+            int pos =0;
+            ListNode fast = head;
+            ListNode slow = head;
+            //关键点：fast指针每次移动两个节点，slow指针每次移动一个节点，有环的话，一定会在环内相遇。（如果有环，在环内相对于slow来说，fast是一个节点一个节点的靠近slow的，所以fast一定可以和slow重合）
+            
+            while (fast != slow&& fast.Next!=null)
+            {
+                fast = fast.Next.Next;
+                slow = slow.Next;
+            }
+            if (fast == slow)
+            {
+                //关键点：假设从头结点到环形入口节点 的节点数为x。 环形入口节点到 fast指针与slow指针相遇节点 节点数为y。 从相遇节点 再到环形入口节点节点数为 z
+                //那么相遇时： slow指针走过的节点数为: x + y， fast指针走过的节点数： x + y + n (y + z) 为fast指针在环内走了n圈才遇到slow指针， （y+z）为 一圈内节点的个数A。
+                // 因为fast指针是一步走两个节点，slow指针一步走一个节点， 所以 fast指针走过的节点数 = slow指针走过的节点数 * 2：(x + y) * 2 = x + y + n(y + z)
+                //有 x = n (y + z) - y
+                //当 n为1的时候，公式就化解为 x = z，
+                //这就意味着，从头结点出发一个指针，从相遇节点 也出发一个指针，这两个指针每次只走一个节点， 那么当这两个指针相遇的时候就是 环形入口的节点。
+                //那么 n如果大于1是什么情况呢，就是fast指针在环形转n圈之后才遇到 slow指针。
+                //其实这种情况和n为1的时候 效果是一样的，一样可以通过这个方法找到 环形的入口节点，只不过，index1 指针在环里 多转了(n - 1)圈，然后再遇到index2，相遇点依然是环形的入口节点。
+                ListNode index1 = fast;
+                ListNode index2 = head;
+                while (index1 != index2)
+                {
+                    pos++;
+                    index1 = index1.Next;
+                    index2=index2.Next;
+                }
+            }
+            else
+            {
+                return -1;
+            }
+            return null;
         }
     }
     public class ListNode
