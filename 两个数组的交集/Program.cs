085a3e6e0e0c37db1ab03619d62449fb5c873ad6
luliangliang <l1970374121@gmail.com>
2023-12-25 18:36:47 +0800

    添加哈希表/数组交集，查找常用字符

---
+++ 两个数组的交集/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace 两个数组的交集
+{
+    internal class Program
+    {
+        #region 问题描述
+        /*给定两个数组 nums1 和 nums2 ，返回 它们的交集 。输出结果中的每个元素一定是 唯一 的。我们可以 不考虑输出结果的顺序 。
+         * 示例 1：
+         * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+         * 输出：[2]
+         * 示例 2：
+         * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+         * 输出：[9,4]
+         * 解释：[4,9] 也是可通过的
+         */
+        #endregion
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public int[] Solution(int[] nums1, int[] nums2)
+        {
+           if (nums1==null||nums1.Length==0||nums2==null||nums2.Length==0)
+            {
+                return new int[0];
+            }
+            HashSet<int> hash1=new HashSet<int>();
+            HashSet<int> hash2=new HashSet<int>();
+           for (int i = 0; i < nums1.Length; i++)
+            {
+                hash1.Add(nums1[i]);
+            }
+           for (int i = 0;i < nums2.Length; i++)
+            {
+                hash2.Add(nums2[i]);
+            }
+            //修改当前的 HashSet<T> 对象，以仅包含该对象和指定集合中存在的元素。
+            hash1.IntersectWith(hash2);
+            return hash1.ToArray();
+        }
+    }
+}
---
+++ 查找常用字符/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace 查找常用字符
+{
+    internal class Program
+    {
+        #region 问题描述
+        /*给你一个字符串数组 words ，请你找出所有在 words 的每个字符串中都出现的共用字符（ 包括重复字符），并以数组形式返回。你可以按 任意顺序 返回答案。
+        示例 1：
+        输入：words = ["bella","label","roller"] 输出：["e","l","l"]
+        示例 2：
+        输入：words = ["cool","lock","cook"] 输出：["c","o"]
+         */
+        #endregion
+        static void Main(string[] args)
+        {
+            string[] A = { "bella", "label", "roller" };
+            List<Char> chars = Solution(A);
+            Console.WriteLine("Hello, World!");
+        }
+        public static List<Char> Solution(string[] words)
+        {
+            //第一个单词进行初始化
+            int[] hash=new int[26];
+            int[] otherStrHash=new int[26];
+            for (int i = 0; i < words[0].Length; i++)
+            {
+                //用‘a’ 来对表示第一个单词的表进行初始化
+                hash[words[0][i] - 'a']++;
+            }
+            for (int i = 1;i<words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    //将剩余所有字符串都初始化存入
+                    otherStrHash[words[i][j] - 'a']++;
+                }
+            }
+            List<Char> result=new List<char>();
+            for(int i = 0;i<26;i++)
+            {
+                if (otherStrHash[i]>= words.Length-1)
+                {
+                    hash[i] = Math.Min(hash[i], otherStrHash[i]);
+                }
+                else
+                {
+                    hash[i] =0;
+                }
+
+                while (hash[i]!=0)
+                {
+                    result.Add((char)(i + 'a'));
+                    hash[i]--;
+                }
+            }
+            return result;
+        }
+    }
+}
