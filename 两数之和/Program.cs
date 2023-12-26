ceca1db846874035ae83e986c565aae81f004c3e
luliangliang <l1970374121@gmail.com>
2023-12-26 17:09:16 +0800

    添加快乐数与两数之和（部分）

---
+++ 两数之和/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace 两数之和
+{
+    internal class Program
+    {
+        #region 问题描述
+        /*给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+         * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+         * 示例:
+         * 给定 nums = [2, 7, 11, 15], target = 9
+         * 因为 nums[0] + nums[1] = 2 + 7 = 9
+         * 所以返回 [0, 1]
+         */
+        #endregion
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public void Solution(int[] nums, int target)
+        {
+            HashMap<int,int> visited = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int n = target - nums[i];
+                if (!visited.Contains(n))
+                {
+                    visited.Add(n);
+                }
+            }
+        }
+    }
+}
---
+++ 快乐数/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace 快乐数
+{
+    #region 问题描述
+    /*「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数。
+        如果 n 是快乐数就返回 True ；不是，则返回 False 。
+        示例：
+        输入：19
+        输出：true
+        解释：
+        1^2 + 9^2 = 82
+        8^2 + 2^2 = 68
+        6^2 + 8^2 = 100
+        1^2 + 0^2 + 0^2 = 1
+     */
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public bool Solution(int n)
+        {
+            //关键：不是快乐数会出现循环
+            HashSet<int> visited = new HashSet<int>();
+            while (n!=1&&visited.Contains(n))
+            {
+                visited.Add(n);
+                n=GetSum(n);
+            }
+           //n！=1说明存在循环，不是快乐数
+            return n==1;
+        }
+
+        private  int GetSum( int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                //计算每个位数上的平方和
+                sum += (n % 10) * (n % 10);
+                n = n / 10;
+            }
+            return sum;
+        }
+    }
+}
